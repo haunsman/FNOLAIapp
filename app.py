ed1d2e8a84dbca9dcaadc6bc2810b065d7cdab8d
haunsman <creightonfowler@gmail.com>
2023-06-12 16:42:13 -0500

    Revert "Implement API into"
    This reverts commit 78c30ff4d251c62f80faeaf16797c8c2a4eb6cde.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 import os
 import processor
@@ -24,35 +24,66 @@ client = session.client('s3',
                         aws_access_key_id='DO00PUNYAAR7YQ8DA4CG',
                         aws_secret_access_key='UdfymTzv4m0YsEnBhE0R0g0WZOcbX4yrx49EPTNZbJ0')
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route('/upload', methods=['POST'])
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'})
-    file = request.files['file']
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'})
-    claim_number = request.form.get('claim_number')
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        filename_with_claim_id = f"{os.path.splitext(filename)[0]}CID{claim_number}{os.path.splitext(filename)[1]}"
-        s3_key = f"uploads/{filename_with_claim_id}"
-        file_contents = file.read()
-        client.put_object(Bucket=S3_BUCKET_NAME, Key=s3_key, Body=file_contents)
-        return jsonify({'filename': filename_with_claim_id})
-    return jsonify({'error': 'Invalid file'})
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return 'No file part'
+        file = request.files['file']
+        if file.filename == '':
+            return 'No selected file'
+        claim_number = request.form.get('claim_number')
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+
+            # Append claim ID to the filename
+            filename_with_claim_id = f"{os.path.splitext(filename)[0]}CID{claim_number}{os.path.splitext(filename)[1]}"
+
+            # Upload file to S3 bucket
+            s3_key = f"uploads/{filename_with_claim_id}"
+            file_contents = file.read()
+            client.put_object(Bucket=S3_BUCKET_NAME, Key=s3_key, Body=file_contents)
+
+            return redirect(url_for('upload_file'))
+
+    # Get the list of files from the S3 bucket
+    response = client.list_objects_v2(Bucket=S3_BUCKET_NAME, Prefix='uploads/')
+    files = [obj['Key'].split('/')[-1] for obj in response['Contents']] if 'Contents' in response else []
+
+    return render_template('upload.html', files=files)
+
 
 @app.route('/process', methods=['POST'])
 def process_file():
     filename = request.form['filename']
     file_url = url_for('upload_file', filename=filename, _external=True)
-    file_url = file_url.replace("?filename=", "").replace("=", "")
+    file_url = file_url.replace("?filename=", "")
+    file_url = file_url.replace("=", "")
     result = processor.process_file(file_url)
+
+    # Delete the file from the S3 bucket
     s3_key = f"uploads/{filename}"
     client.delete_object(Bucket=S3_BUCKET_NAME, Key=s3_key)
-    return jsonify(result)
+
+    # Convert the result dictionary to a JSON string with double quotes for property names
+    result_json = json.dumps(result, ensure_ascii=False)
+    # Pass the JSON string to the processing_result template
+    return redirect(url_for('processing_result', result=result_json))
+
+
+@app.route('/processing_result')
+def processing_result():
+    result_json = request.args.get('result')
+    # Parse the JSON string back into a dictionary
+    result = json.loads(result_json)
+    return render_template('processing_result.html', result=result)
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
