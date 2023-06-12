faa20f63473330c07ee55013f80338caa63f2d44
haunsman <creightonfowler@gmail.com>
2023-06-12 00:22:19 -0500

    Testing spaces fill storage.

--- app.py
+++ app.py
@@ -2,23 +2,33 @@ from flask import Flask, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 import os
 import processor
-from flask import Flask
-from flask_bootstrap import Bootstrap
 import json
-
+import boto3
+import botocore
 
 app = Flask(__name__)
 
-# Configure upload
-UPLOAD_FOLDER = '/Users/creightonfowler/Desktop/FNOLProcess/uploads'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB limit
+# Configure AWS S3
+S3_BUCKET_NAME = 'fnol-files'
+S3_REGION = 'nyc3'
+S3_ACCESS_KEY = 'DO00PUNYAAR7YQ8DA4CG'
+S3_SECRET_KEY = 'UdfymTzv4m0YsEnBhE0R0g0WZOcbX4yrx49EPTNZbJ0'
+
+session = boto3.session.Session()
+client = session.client('s3',
+                        config=botocore.config.Config(s3={'addressing_style': 'virtual'}),
+                        region_name='nyc3',
+                        endpoint_url='https://fnol-files.nyc3.digitaloceanspaces.com',
+                        aws_access_key_id=os.getenv('DO00PUNYAAR7YQ8DA4CG'),
+                        aws_secret_access_key=os.getenv('UdfymTzv4m0YsEnBhE0R0g0WZOcbX4yrx49EPTNZbJ0'))
+
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -31,43 +41,34 @@ def upload_file():
         claim_number = request.form.get('claim_number')
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            
+
             # Append claim ID to the filename
             filename_with_claim_id = f"{os.path.splitext(filename)[0]}CID{claim_number}{os.path.splitext(filename)[1]}"
-            
-            # Save the renamed file
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename_with_claim_id))
-            
+
+            # Upload file to S3 bucket
+            s3_key = f"default/{filename_with_claim_id}"
+            file_contents = file.read()
+            s3_client.put_object(Bucket=S3_BUCKET_NAME, Key=s3_key, Body=file_contents)
+
             return redirect(url_for('upload_file'))
 
-    files = os.listdir(app.config['UPLOAD_FOLDER'])
-    return render_template('upload.html', files=files)
+    return render_template('upload.html')
 
 
 @app.route('/process', methods=['POST'])
 def process_file():
-    filename = request.form['filename']
-    file_url = url_for('upload_file', filename=filename, _external=True)
-    file_url = file_url.replace("?filename=", "")
-    file_url = file_url.replace("=", "")
-    result = processor.process_file(file_url)
-    
-    # Delete the file
-    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    os.remove(file_path)
-    
-    # Convert the result dictionary to a JSON string with double quotes for property names
-    result_json = json.dumps(result, ensure_ascii=False)
-    # Pass the JSON string to the processing_result template
-    return redirect(url_for('processing_result', result=result_json))
+    # Process the file (assuming you have the necessary logic in the processor module)
+    # ...
+
+    return redirect(url_for('processing_result'))
 
 
 @app.route('/processing_result')
 def processing_result():
-    result_json = request.args.get('result')
-    # Parse the JSON string back into a dictionary
-    result = json.loads(result_json)
-    return render_template('processing_result.html', result=result)
+    # Get the processing result (assuming you have the necessary logic)
+    # ...
+
+    return render_template('processing_result.html')
 
 
 if __name__ == "__main__":
