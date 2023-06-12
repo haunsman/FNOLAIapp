4efebf7295b4ea7aac3b6a4ff419811edfe5b05b
haunsman <creightonfowler@gmail.com>
2023-06-12 00:43:52 -0500

    Fixed error for endpoint_url

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ session = boto3.session.Session()
 client = session.client('s3',
                         config=botocore.config.Config(s3={'addressing_style': 'virtual'}),
                         region_name='nyc3',
-                        endpoint_url='https://fnol-files.nyc3.digitaloceanspaces.com',
+                        endpoint_url='https://nyc3.digitaloceanspaces.com',
                         aws_access_key_id='DO00PUNYAAR7YQ8DA4CG',
                         aws_secret_access_key='UdfymTzv4m0YsEnBhE0R0g0WZOcbX4yrx49EPTNZbJ0')
 
